cec000ce16bf991b9fea7a8d4a62c058bff6d983
Francesco Di Sales <f.disales@hotmail.com>
2022-09-11 18:39:50 +0200

    Create window.cpp

---
+++ core/window/window.cpp
@@ -0,0 +1,55 @@
+
+
+#include "window.h"
+
+#include<string>
+#include"raylib.h"
+
+window::window(int screenWidth,int screenHeight,std::string titleWindow)
+{
+	this->screenWidth = screenWidth;
+	this->screenHeight = screenHeight;
+
+	this->titleWindow = titleWindow;
+
+}
+
+void window::init()
+{
+	InitWindow(this -> screenWidth, this ->screenHeight, titleWindow.c_str());
+}
+
+void window::setFPS(int fps)
+{
+	SetTargetFPS(fps);
+}
+
+void window::beginDrawing()
+{
+	BeginDrawing();
+}
+
+void window::endDrawing()
+{
+	EndDrawing();
+}
+
+void window::setBackgroundPlain(CLITERAL(Color) color)
+{
+	ClearBackground(color);
+}
+
+void window::drawTextWindow(std::string text,int xPosition,int yPosition,int fontSize,CLITERAL(Color) color)
+{
+	DrawText(text.c_str(),xPosition,yPosition,fontSize,LIGHTGRAY);
+}
+
+bool window::isWindowsClosed()
+{
+	return WindowShouldClose();
+}
+
+void window::close()
+{
+	CloseWindow();
+}
