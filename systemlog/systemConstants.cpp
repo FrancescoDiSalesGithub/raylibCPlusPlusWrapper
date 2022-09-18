72a80b5a015c70161c04f2d9c7dd93eb90c61cfa
FrancescoDiSalesGithub <f.disales@hotmail.com>
2022-09-18 17:07:04 +0200

    adding log functionality

---
+++ systemlog/systemConstants.cpp
@@ -0,0 +1,5 @@
+
+#include "systemConstants.h"
+
+
+
---
+++ systemlog/systemConstants.h
@@ -0,0 +1,19 @@
+
+
+#ifndef SYSTEMLOG_SYSTEMCONSTANTS_H_
+#define SYSTEMLOG_SYSTEMCONSTANTS_H_
+
+#include<string>
+
+namespace SYSCONSTANTSRAY
+{
+	class systemConstants
+	{
+		public:
+			std::string WRONG_GIF_POINTER = "Pointer is null or not assigned with & operator";
+	};
+
+}
+
+
+#endif /* SYSTEMLOG_SYSTEMCONSTANTS_H_ */
