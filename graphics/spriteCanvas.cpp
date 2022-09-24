3f794d8a90bdfb56b861a12eb2722c4e2a051370
Francesco Di Sales <f.disales@hotmail.com>
2022-09-24 15:04:52 +0200

    Update spriteCanvas.cpp

--- graphics/spriteCanvas.cpp
+++ graphics/spriteCanvas.cpp
@@ -1,22 +1,40 @@
 
 #include "spriteCanvas.h"
+#include"raylib.h"
+
 
 using namespace SPRITERAYCPP;
+
+spriteCanvas::spriteCanvas()
+{
+
+}
 
 spriteCanvas::spriteCanvas(std::string filename)
 {
 	this->filename = filename;
-	this->texture2D = LoadTexture(this->filename.c_str());
 }
 
-spriteCanvas::~spriteCanvas()
+
+void spriteCanvas::initTexture2D()
 {
-	UnloadTexture(this->texture2D);
+    this->texture2D = LoadTexture(this->filename.c_str());
 }
+
+void spriteCanvas::unloadTexture2D()
+{
+    UnloadTexture(this->texture2D);
+}
+
 
 void spriteCanvas::draw(Vector2 position,Color color)
 {
 	DrawTexture(this->texture2D, position.x, position.y, color);
+}
+
+void spriteCanvas::drawRec(Vector2 position,Rectangle rec)
+{
+    DrawTextureRec(this->texture2D,rec,position,WHITE);
 }
 
 void spriteCanvas::setFileName(std::string filename)
